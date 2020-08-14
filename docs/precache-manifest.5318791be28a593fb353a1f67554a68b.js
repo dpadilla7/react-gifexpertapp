11d9b1cd67c637b5ad10637079ebcf55d6ebafb4
Dean Padilla Herrera <d3anpadi@gmail.com>
2020-08-14 17:40:17 -0500

    docs agregado

---
+++ docs/precache-manifest.5318791be28a593fb353a1f67554a68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ec5d346165797324d00cb78ad790dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a464b6a7cd2fe1606c9",
+    "url": "/static/css/main.10258b01.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a464b6a7cd2fe1606c9",
+    "url": "/static/js/main.a609156d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
